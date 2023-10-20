f32106e2bf89294b668af2fc399183623191fbbb
McMartinezc <psyence_mc@hotmail.com>
2023-10-20 12:58:56 +0200

    suma

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ let secondNumber = chance.integer({ min: 0, max: 10 })
 
 //get the total
 let total = firstNumber + secondNumber;
+
+const num1= document.getElementById('primary-number');
+const num2= document.getElementById('secondary-number');
+const btn = document.getElementById('btn');
+const message = document.getElementById('message');
+
+num1.textContent = firstNumber;
+num2.textContent = secondNumber;
+
+btn.addEventListener("click", ()=>{
+    const input = document.querySelector('#guess').value;
+    
+    if(+input === total){
+        message.textContent = "Es correcto!"
+    }else{
+        message.textContent = `No es correcto el resultado es ${total}`
+    }
+})
